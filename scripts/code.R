9d9bb05b90ea52b59b4de6552c4c9b59712ea771
Isaacson <isaac.michel224@gmail.com>
2026-09-05 00:44:07 -0400

    updated READme and code

--- scripts/code.R
+++ scripts/code.R
@@ -16,8 +16,8 @@ lapply(my_packages, library, character.only = TRUE)
 #Invasice Risk
 
 
-#Let's focus on 4 countries in the region most vulnerable
-#to natural disasters: Hait, Cuba, Honduras & Guatemala 
+#Let's focus on  countries in the LATAM & Caribbean region most vulnerable
+#to natural disasters: 
 
 
 #Re-assessing the Invasiveness of the Land & Geography
