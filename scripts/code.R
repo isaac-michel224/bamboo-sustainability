04c2f942d2bf5897337dedaafd735fe8541b62c1
Isaacson <isaac.michel224@gmail.com>
2026-09-05 00:01:44 -0400

    updated code with comments

--- scripts/code.R
+++ scripts/code.R
@@ -8,3 +8,10 @@ my_packages <- c("tidyverse")
 
 # Load them all at once
 lapply(my_packages, library, character.only = TRUE)
+
+
+#Work in Progress
+
+
+#Let's focus on 4 countries in the region most vulnerable
+#to natural disasters: Hait, Cuba, Honduras & Guatemala 
