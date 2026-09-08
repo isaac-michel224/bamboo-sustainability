9f4ad9bfb44111b255093cafe84ad52a7df01aec
Isaacson <isaac.michel224@gmail.com>
2026-09-07 23:14:58 -0400

    added a line of code to review

--- scripts/code.R
+++ scripts/code.R
@@ -12,6 +12,8 @@ library(BIEN)
 #Finding Bamboo Species in the Americas
 
 #Invasive Risk
+vignette("BIEN_tutorial") #Learnign how to use the 'BIEN' library for project
+
 
 
 #Let's focus on  countries in the LATAM & Caribbean region most vulnerable
