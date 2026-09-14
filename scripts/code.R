62f37987304c37f5617792a0cb9ff6ec536733db
Isaacson <isaac.michel224@gmail.com>
2026-09-13 21:40:11 -0400

    added visualization folder and picture of maps

--- scripts/code.R
+++ scripts/code.R
@@ -32,7 +32,6 @@ arthro_traits <- BIEN_trait_genus(genus = "Arthrostylidium")
 #Occurrence data for the genus 'Guadua' bamboo
 guadua_data <- BIEN_occurrence_genus(genus = "Guadua")
 
-
 # Plot occurrence points on a map zooming in on the Americas
 ggplot(guadua_data, aes(x = longitude, y = latitude)) +
   borders("world", color = "gray85", fill = "gray90") +
@@ -40,36 +39,67 @@ ggplot(guadua_data, aes(x = longitude, y = latitude)) +
   coord_sf(xlim = c(-105, -35), ylim = c(-55, 25)) +
   theme_minimal() +
   labs(title = "Occurrence Records for Genus Guadua",
+       color = "Botanical Genus",
        x = "Longitude", y = "Latitude",
        caption = "Data source: BIEN")
 
-#Plotting Guadua amplexifolia bamboo in the Americas
-guadua_amplexifolia_range <- BIEN_ranges_load_species(species = "Guadua amplexifolia")
+#ggsave("visualizations/guadua.png")
 
-map('world', fill = TRUE,
-    col = "grey",
-    bg = "light blue",
-    xlim = c(-180, -20),
-    ylim = c(-60, 80))
 
-plot(guadua_amplexifolia_range[1],
-     col = "green",
-     add = TRUE)
 
-#Plotting Guadua Species Data on Map
+#Occurrence data for Chusquea genus
+chus_data <- BIEN_occurrence_genus(genus = "Chusquea")
 
-monkey_puzzle <- BIEN_ranges_load_species(species = "Guadua trinii")
-
-world_map <- map_data("world")
-
-ggplot() +
-  geom_polygon(data = world_map, aes(x = long, y = lat, group = group),
-               fill = "lightgrey", color = "white") +
-  geom_sf(data = monkey_puzzle, fill = "forestgreen", alpha = 0.6) +
-  coord_sf(xlim = c(-80, -35), ylim = c(-60, 5)) + 
+ggplot(chus_data, aes(x = longitude, y = latitude)) +
+  borders("world", color = "gray85", fill = "gray90") +
+  geom_point(colour = "darkgreen", size =1, alpha = 0.6) +
+  coord_sf(xlim = c(-105, -35), ylim = c(-55, 25)) +
   theme_minimal() +
-  labs(title = "Species Range Map: Guadua trinii",
+  labs(title = "Occurrence Records for Genus Chusquea",
+       color = "Botanical Genus",
+       x = "Longitude", y = "Latitude",
        caption = "Data source: BIEN")
+
+#ggsave("visualizations/chusquea.png")
+
+
+
+#Invasive Risk
+#Read: https://www.google.com/search?q=assessing+invasive+species+data+in+r&sca_esv=e76e82dc2fdda140&sxsrf=APpeQntnPRzks11x49vxFKdA1_VhjHm7ag%3A1789349421107&source=hp&ei=LU6naqOJBMD-ptQP9d-d2Ak&iflsig=ABILxe8AAAAAaqdcPVtJqkkQ60y16Z3zvsIuGilSJ4Zl&oq=assessing+invasive+&gs_lp=Egdnd3Mtd2l6IhNhc3Nlc3NpbmcgaW52YXNpdmUgKgUIARigATIFECEYoAEyBRAhGKABMgUQIRigATIFECEYoAEyBRAhGKsCMgUQIRifBTIFECEYnwUyBRAhGJ8FMgUQIRifBTIFECEYnwVImkJQAFilNnADeACQAQCYAXWgAdYQqgEEMTQuOLgBA8gBAPgBAZgCGaACgxLCAgQQIxgnwgILEAAYgAQYigUYkQLCAhEQLhiABBixAxiDARjHARjRA8ICDhAuGIAEGIoFGLEDGIMBwgIREC4YgwEYxwEYsQMY0QMYgATCAg4QABiABBiKBRixAxiDAcICDhAuGIAEGMcBGK8BGI4FwgIKEC4YgAQYigUYQ8ICCxAAGIAEGLEDGIMBwgIIEAAYgAQYsQPCAhEQABiABBiKBRiNBhixAxiDAcICChAAGIAEGIoFGEPCAgUQLhiABMICDRAuGIAEGIoFGEMYsQPCAhAQABiABBiKBRhDGLEDGIMBwgINEAAYgAQYigUYQxixA8ICBRAAGIAEwgIOEC4YrwEYxwEYgAQYjgXCAggQABiABBi0B8ICBxAAGIAEGArCAgYQABgWGB7CAgsQABiABBiKBRiGA8ICBRAAGO8FwgIIEAAYiQUYogTCAgcQIRgKGKABwgIHECEYqwIYCpgDAOIDBRIBMSBAkgcFMTMuMTKgB8y_AbIHBTEwLjEyuAfwEcIHCDAuNy4xNy4xyAdngAgB&sclient=gws-wiz
+
+
+
+
+
+
+
+# #Plotting Guadua amplexifolia bamboo in the Americas
+# guadua_amplexifolia_range <- BIEN_ranges_load_species(species = "Guadua amplexifolia")
+# 
+# map('world', fill = TRUE,
+#     col = "grey",
+#     bg = "light blue",
+#     xlim = c(-180, -20),
+#     ylim = c(-60, 80))
+# 
+# plot(guadua_amplexifolia_range[1],
+#      col = "green",
+#      add = TRUE)
+# 
+# #Plotting Guadua Species Data on Map
+# 
+# monkey_puzzle <- BIEN_ranges_load_species(species = "Guadua trinii")
+# 
+# world_map <- map_data("world")
+# 
+# ggplot() +
+#   geom_polygon(data = world_map, aes(x = long, y = lat, group = group),
+#                fill = "lightgrey", color = "white") +
+#   geom_sf(data = monkey_puzzle, fill = "forestgreen", alpha = 0.6) +
+#   coord_sf(xlim = c(-80, -35), ylim = c(-60, 5)) + 
+#   theme_minimal() +
+#   labs(title = "Species Range Map: Guadua trinii",
+#        caption = "Data source: BIEN")
 
 
 #sgenus_ranges <- BIEN_ranges_load_species(BIEN_metadata_match_data("Guadua")$species)
@@ -87,11 +117,6 @@ ggplot() +
 # 
 # head(latam_carib)
 
-
-
-
-
-#Invasive Risk
 
 
 
