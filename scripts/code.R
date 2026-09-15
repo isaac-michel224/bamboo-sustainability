ea449e8d49ab6ded984ec5d6456860bf8f224b30
Isaacson <isaac.michel224@gmail.com>
2026-09-14 23:23:53 -0400

    added another visualization map

--- scripts/code.R
+++ scripts/code.R
@@ -62,9 +62,21 @@ ggplot(chus_data, aes(x = longitude, y = latitude)) +
 
 #ggsave("visualizations/chusquea.png")
 
+arthro_data <- BIEN_occurrence_genus(genus = "Arthrostylidium")
+
+ggplot(arthro_data, aes(x = longitude, y = latitude)) +
+  borders("world", color = "gray85", fill = "gray90") +
+  geom_point(colour = "darkgreen", size =1, alpha = 0.6) +
+  coord_sf(xlim = c(-105, -35), ylim = c(-55, 25)) +
+  theme_minimal() +
+  labs(title = "Occurrence Records for Genus Arthrostylidium",
+       color = "Botanical Genus",
+       x = "Longitude", y = "Latitude",
+       caption = "Data source: BIEN")
 
 
-#Next Step: Invasive Risk
+
+#Next Step: Re-assessing the Invasive Risk of the Land & Geography
 #Read: https://www.google.com/search?q=assessing+invasive+species+data+in+r&sca_esv=e76e82dc2fdda140&sxsrf=APpeQntnPRzks11x49vxFKdA1_VhjHm7ag%3A1789349421107&source=hp&ei=LU6naqOJBMD-ptQP9d-d2Ak&iflsig=ABILxe8AAAAAaqdcPVtJqkkQ60y16Z3zvsIuGilSJ4Zl&oq=assessing+invasive+&gs_lp=Egdnd3Mtd2l6IhNhc3Nlc3NpbmcgaW52YXNpdmUgKgUIARigATIFECEYoAEyBRAhGKABMgUQIRigATIFECEYoAEyBRAhGKsCMgUQIRifBTIFECEYnwUyBRAhGJ8FMgUQIRifBTIFECEYnwVImkJQAFilNnADeACQAQCYAXWgAdYQqgEEMTQuOLgBA8gBAPgBAZgCGaACgxLCAgQQIxgnwgILEAAYgAQYigUYkQLCAhEQLhiABBixAxiDARjHARjRA8ICDhAuGIAEGIoFGLEDGIMBwgIREC4YgwEYxwEYsQMY0QMYgATCAg4QABiABBiKBRixAxiDAcICDhAuGIAEGMcBGK8BGI4FwgIKEC4YgAQYigUYQ8ICCxAAGIAEGLEDGIMBwgIIEAAYgAQYsQPCAhEQABiABBiKBRiNBhixAxiDAcICChAAGIAEGIoFGEPCAgUQLhiABMICDRAuGIAEGIoFGEMYsQPCAhAQABiABBiKBRhDGLEDGIMBwgINEAAYgAQYigUYQxixA8ICBRAAGIAEwgIOEC4YrwEYxwEYgAQYjgXCAggQABiABBi0B8ICBxAAGIAEGArCAgYQABgWGB7CAgsQABiABBiKBRiGA8ICBRAAGO8FwgIIEAAYiQUYogTCAgcQIRgKGKABwgIHECEYqwIYCpgDAOIDBRIBMSBAkgcFMTMuMTKgB8y_AbIHBTEwLjEyuAfwEcIHCDAuNy4xNy4xyAdngAgB&sclient=gws-wiz
 
 
@@ -87,7 +99,3 @@ ggplot(chus_data, aes(x = longitude, y = latitude)) +
 #to natural disasters: 
 
 
-#Re-assessing the Invasive Risk of the Land & Geography
-
-
-#---------------------------------------------------------------------------+
