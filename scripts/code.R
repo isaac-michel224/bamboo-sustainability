6abba99769bb7932b925b2d240c85c76ce0d198d
Isaacson <isaac.michel224@gmail.com>
2026-09-17 23:47:37 -0400

    removed dataset

--- scripts/code.R
+++ scripts/code.R
@@ -97,7 +97,8 @@ ggplot(aulo_data, aes(x = longitude, y = latitude)) +
 #Next Step: Re-assessing the Invasive Risk of the Land & Geography
 
 #Read GBIF CSV file into dataset to check
-gbif <- read.csv("0002603-250525065834625.csv")
+library(rgbif)
+
 
 
 #Let's focus on  countries in the LATAM & Caribbean region most vulnerable
